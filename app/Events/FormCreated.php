105667a522718f04594c5ee6aca540b271c0f17f
Patrick <patrick.eldira.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Fixed code sniffer issue.

--- app/Events/FormCreated.php
+++ app/Events/FormCreated.php
@@ -13,7 +13,9 @@ use Illuminate\Queue\SerializesModels;
 
 class FormCreated
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 
     /**
      * @var Form
