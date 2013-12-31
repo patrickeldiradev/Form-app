8c9d20213a16c109999fac1c398257eec50ccf24
Patrick <patrick.eldira.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Created Enum.

---
+++ app/Enum/FormItemTypeEnum.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Enum;
+
+class FormItemTypeEnum
+{
+    const PAGE = 'page';
+    const PAGE_MODEL = 'App\Models\Page';
+    const SECTION = 'section';
+    const SECTION_MODEL = 'App\Models\Section';
+    const QUESTION = 'question';
+    const QUESTION_MODEL = 'App\Models\Question';
+    const FORM = 'form';
+    const FORM_MODEL = 'App\Models\Form';
+
+    /**
+     * @param $type
+     * @return string
+     */
+    public static function getType($type): string
+    {
+        return static::availableTypes()[$type];
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function availableTypes(): array
+    {
+        return [
+            static::PAGE => static::PAGE_MODEL,
+            static::SECTION => static::SECTION_MODEL,
+            static::QUESTION => static::QUESTION_MODEL,
+            static::FORM => static::FORM_MODEL,
+        ];
+    }
+}
