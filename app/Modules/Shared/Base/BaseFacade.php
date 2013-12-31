987ed1b41e0223fc803f695189c5d6c9a710cb71
Patrick <patrick.eldira.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Use singleton to resolve factory.

--- app/Modules/Shared/Base/BaseFacade.php
+++ app/Modules/Shared/Base/BaseFacade.php
@@ -4,10 +4,20 @@ namespace App\Modules\Shared\Base;
 
 class BaseFacade
 {
+    public $factory;
     /**
      * @return mixed
      */
     public function getFactory(): mixed
+    {
+        if(!$this->factory) {
+            $this->factory = $this->resolveFactory();
+        }
+
+        return $this->factory;
+    }
+
+    public function resolveFactory()
     {
         $namespace = (new \ReflectionClass($this))->getNamespaceName();
         $strArray = explode('\\', $namespace, 4);
