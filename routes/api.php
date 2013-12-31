4a2b659426e26bdc6e122a56f12298efa4a380b3
Patrick <patrick.eldira.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Created Routes.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
+    Route::post('form', 'FormsController@storeForm');
+    Route::get('form/{form}', 'FormsController@getForm');
+    Route::post('questionnaire', 'FormsController@storeQuestionnaire');
 });
